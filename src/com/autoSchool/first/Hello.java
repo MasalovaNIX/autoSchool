0a010d166449b4f61d743c117abb3083a825ab52
MasalovaNIX <82531938+MasalovaNIX@users.noreply.github.com>
2021-04-21 17:05:36 +0300

    Working Hello

--- src/com/autoSchool/first/Hello.java
+++ src/com/autoSchool/first/Hello.java
@@ -1,9 +1,20 @@
 package com.autoSchool.first;
+import java.util.Scanner;
 
 public class Hello {
-    public static void main(String[] args)
 
-    {
-        System.out.println("Hello World!");
-    }
+            public static void printHelloName() {
+            Scanner in = new Scanner(System.in);
+            System.out.print("Введите имя: ");
+            String name = in.nextLine();
+            System.out.println("Hello," + name + "!");
+
+        }
+
+        public static void hello() {
+            System.out.println("Hello World!");
+
+        }
 }
+
+
---
+++ src/com/autoSchool/first/main.java
@@ -0,0 +1,10 @@
+package com.autoSchool.first;
+
+public class main {
+
+    public static void main(String[] args){
+        Hello.hello();
+        Hello.printHelloName();
+
+    }
+}
